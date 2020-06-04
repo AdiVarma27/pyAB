70af1002eb1d4c2ba24c7bf55b2e7b46dcd5fd32
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-04 18:49:08 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Aditya Varma Kalidindi'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
