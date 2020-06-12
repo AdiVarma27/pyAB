181d2f91f571c84717bba8ef09a209b25090bc97
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-11 22:33:47 -0400

    checkin

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 import os
 from setuptools import setup, find_packages
 
-curr_path = os.path.abspath(os.path.dirname(__file__))
-
-files = os.listdir(os.curdir)
-
-with open('requirements.txt', encoding="utf-8") as req_file:
+import os
+cwd = os.getcwd()
+with open(cwd + '/requirements.txt', encoding="utf-8") as req_file:
     REQUIRED_PACKAGES = [line.rstrip() for line in req_file]
 
 
 setup(name='pyAB',
-      version='5.1.4',
+      version='5.1.6',
       description='A/B Testing using Bayesian & Frequentist Statistics',
       url='https://github.com/AdiVarma27/pyAB',
       author='Aditya Varma Kalidindi',
