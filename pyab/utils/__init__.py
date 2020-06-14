a7976b779ceed8d631cf7910824ade55fd63ff60
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-14 14:03:18 -0400

    checkin

--- pyab/utils/__init__.py
+++ pyab/utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import check_data_input, check_t_test
+from .utils import check_data_input, check_t_test, check_valid_parameter
 
 __all__ = [check_data_input, check_t_test, check_valid_parameter]
