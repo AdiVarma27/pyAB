b9b5ebaccce7ca4adeb7e2a2b697f3531b45efae
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-14 21:08:29 -0400

    v0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='pyAB',
-      version='10.3.9',
+      version='0.0.1',
       description='A/B Testing using Bayesian & Frequentist Statistics',
       url='https://github.com/AdiVarma27/pyAB',
       author='Aditya Varma Kalidindi',
