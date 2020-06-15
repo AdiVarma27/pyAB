b812f360491887eaeb23e802920cdcd833cfa64c
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-14 21:02:49 -0400

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pyAB',
-      version='10.3.4',
+      version='10.3.9',
       description='A/B Testing using Bayesian & Frequentist Statistics',
       url='https://github.com/AdiVarma27/pyAB',
       author='Aditya Varma Kalidindi',
       author_email='kadityavarma27@gmail.com',
-      install_requires = ['matplotlib', 'numpy>=1.13', 'scipy==1.2.0', 'seaborn==0.9.0', 'statsmodels==0.9.0']
+      packages=find_packages()
     )
