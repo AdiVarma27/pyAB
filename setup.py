91607b69eb0f54e1554f9d209e71f473203f47eb
AdiVarma27 <kadityavarma27@gmail.com>
2020-06-17 17:01:58 -0400

    req check

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='pyAB',
-      version='0.0.1',
+      version='20.0.1',
       description='A/B Testing using Bayesian & Frequentist Statistics',
       url='https://github.com/AdiVarma27/pyAB',
       author='Aditya Varma Kalidindi',
       author_email='kadityavarma27@gmail.com',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires = ['matplotlib','numpy>=1.13','scipy==1.2.0','seaborn==0.9.0','statsmodels']
     )
